ee00cf4bd1c76e60f1bf4090ff94912ed6a710fc
Lily Yerim Kim <77142800+lilble@users.noreply.github.com>
2024-06-30 19:49:51 +0900

    refactored wanted-scraper

--- wanted-scraper.py
+++ wanted-scraper.py
@@ -3,66 +3,73 @@ import time
 from bs4 import BeautifulSoup
 import csv
 
+wantedurl = "https://www.wanted.co.kr"
+
 def wait():
     time.sleep(1)
 
-p = sync_playwright().start()
-browser = p.chromium.launch(headless=False)
-page = browser.new_page()
+def scrape_page(page, keyword):
+    # Click search button
+    page.click("button[aria-label=\"검색\"]")
+    wait() 
 
-# Go to wanted
-wantedurl = "https://www.wanted.co.kr"
-page.goto(f"{wantedurl}/wdlist")
-wait()
+    # Fill input box
+    page.get_by_placeholder("검색어를 입력해 주세요.").fill(keyword)
+    wait()
 
-# Click search button
-page.click("button[aria-label=\"검색\"]")
-wait() 
+    # Press enter key
+    page.keyboard.down("Enter")
+    wait()
 
-# Fill input box
-page.get_by_placeholder("검색어를 입력해 주세요.").fill("flutter")
-wait()
+    # Click position tab
+    page.click("#search_tab_position")
+    wait()
 
-# Press enter key
-page.keyboard.down("Enter")
-wait()
+    # Scroll down 3 times
+    page.keyboard.down("End")
+    page.keyboard.down("End")
+    page.keyboard.down("End")
+    wait()
 
-# Click position tab
-page.click("#search_tab_position")
-wait()
+    content = page.content()
+    return content
 
-# Scroll down
-page.keyboard.down("End")
-wait()
-page.keyboard.down("End")
-wait()
-page.keyboard.down("End")
-wait()
+def get_jobs(content):
+    # Get job data
+    jobs_db = []
+    soup = BeautifulSoup(content, "html.parser")
+    jobs_container = soup.find_all("div", {"data-testid" : "SearchPositionListContainer"})[-1]
+    jobs = jobs_container.find_all("div", role="listitem")
+    for job in jobs:
+        a = job.find("a")
+        title_and_company = a.find_all("div")[-1]
+        job_data = {
+            "title": title_and_company.find("strong").text,
+            "company": title_and_company.find("span").find("span").text,
+            "link": wantedurl + a["href"],
+        }
+        jobs_db.append(job_data)
+    return jobs_db
 
-content = page.content()
-p.stop()
+def save_to_csv(keyword, jobs_db):
+    file = open(f"wanted-jobs/{keyword}.csv", "w")
+    writer = csv.writer(file)
+    writer.writerow(["Title", "Company", "Link"])
+    for job in jobs_db:
+        writer.writerow(list(job.values()))
 
-# Get job data
-jobs_db = []
-soup = BeautifulSoup(content, "html.parser")
-jobs_container = soup.find_all("div", {"data-testid" : "SearchPositionListContainer"})[-1]
-jobs = jobs_container.find_all("div", role="listitem")
-for job in jobs:
-    a = job.find("a")
-    title_and_company = a.find_all("div")[-1]
-    job_data = {
-        "title": title_and_company.find("strong").text,
-        "company": title_and_company.find("span").find("span").text,
-        "link": wantedurl + a["href"],
-    }
-    jobs_db.append(job_data)
+def scrape_wanted_with_keywords(keywords):
+    # Launch browser
+    p = sync_playwright().start()
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    # Go to wanted
+    page.goto(f"{wantedurl}/wdlist")
+    wait()
+    for keyword in keywords:
+        jobs = get_jobs(scrape_page(page, keyword))
+        save_to_csv(keyword, jobs)
+    p.stop()
 
-# print(jobs_db)
-# print(len(jobs_db))
-
-# Save to csv
-file = open("jobs.csv", "w")
-writer = csv.writer(file)
-writer.writerow(["Title", "Company", "Link"])
-for job in jobs_db:
-    writer.writerow(list(job.values()))
+keywords = ["flutter", "python", "golang"]
+scrape_wanted_with_keywords(keywords)
