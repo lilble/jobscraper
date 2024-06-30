98976306b994f5a04a04b178c5be33064eb8b28c
Lily Yerim Kim <77142800+lilble@users.noreply.github.com>
2024-06-30 18:59:19 +0900

    scrape wanted with playwright

---
+++ wanted-scraper.py
@@ -0,0 +1,59 @@
+from playwright.sync_api import sync_playwright
+import time 
+from bs4 import BeautifulSoup
+
+def wait():
+    time.sleep(3)
+
+p = sync_playwright().start()
+
+browser = p.chromium.launch(headless=False)
+page = browser.new_page()
+
+wantedurl = "https://www.wanted.co.kr"
+page.goto(f"{wantedurl}/wdlist")
+wait()
+# Click search button
+page.click("button[aria-label=\"검색\"]")
+wait() 
+
+# Fill input box
+page.get_by_placeholder("검색어를 입력해 주세요.").fill("flutter")
+wait()
+
+# Press enter key
+page.keyboard.down("Enter")
+wait()
+
+# Click position tab
+page.click("#search_tab_position")
+wait()
+
+# Scroll down
+page.keyboard.down("End")
+wait()
+page.keyboard.down("End")
+wait()
+page.keyboard.down("End")
+wait()
+
+content = page.content()
+p.stop()
+
+# Get job data
+jobs_db = []
+soup = BeautifulSoup(content, "html.parser")
+jobs_container = soup.find_all("div", {"data-testid" : "SearchPositionListContainer"})[-1]
+jobs = jobs_container.find_all("div", role="listitem")
+for job in jobs:
+    a = job.find("a")
+    title_and_company = a.find_all("div")[-1]
+    job_data = {
+        "title": title_and_company.find("strong").text,
+        "company": title_and_company.find("span").find("span").text,
+        "link": wantedurl + a["href"],
+    }
+    jobs_db.append(job_data)
+
+print(jobs_db)
+print(len(jobs_db))
