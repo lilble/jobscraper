1759bdc89a9865c9514a7c53e6b5660fa51c4124
Lily Yerim Kim <77142800+lilble@users.noreply.github.com>
2024-07-02 01:35:52 +0900

    export

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_file
 from wanted_scraper import get_jobs_with_keyword
 from datetime import datetime, timedelta
+from savetofile import save_to_csv
 
 app = Flask("JobScraper")
 
@@ -16,11 +17,21 @@ def search():
     if not keyword:
         return redirect('/')
     curr_time = datetime.now()
-    if keyword in db and abs((curr_time - db[keyword]["time"]).total_seconds()) > 3600:
-        jobs = db[keyword]
+    if keyword in db and abs((curr_time - db[keyword]["time"]).total_seconds()) < 3600:
+        jobs = db[keyword]["jobs"]
     else:
         jobs = get_jobs_with_keyword(keyword)
         db[keyword] = {"time": datetime.now(), "jobs": jobs}
     return render_template('search.html', keyword=keyword, jobs=jobs)
+
+@app.route('/export')
+def export():
+    keyword = request.args.get("keyword")
+    if not keyword:
+        return redirect('/')
+    if keyword not in db:
+        return redirect(f"/search?keyword={keyword}")
+    save_to_csv(keyword, db[keyword]["jobs"])
+    return send_file(f"wanted-jobs/{keyword}.csv", as_attachment=True)
 
 app.run('0.0.0.0', 8000)
